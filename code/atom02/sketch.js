e5ba9fe58cea35ea9183d43bb969c56ffd4b3d01
BForoughi <147166034+BForoughi@users.noreply.github.com>
2024-05-14 22:26:10 +0100

    adding atom02

---
+++ code/atom02/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(700, 700, WEBGL);
+}
+
+function draw() {
+  background(1000);
+  push();
+  translate(0,0,100)
+  rotateY(frameCount/100)
+  rotateX(frameCount/100)
+  rotateZ(frameCount/50)
+  fill(255, 0, 127)
+  sphere(100)
+  pop();
+
+}
