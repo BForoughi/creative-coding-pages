63a302fff0a4745b8df370680d0ad3fac26fb3e0
BForoughi <147166034+BForoughi@users.noreply.github.com>
2024-05-15 22:55:09 +0100

    adding TrippyParrot02

---
+++ code/TrippyParrot02/sketch.js
@@ -0,0 +1,30 @@
+function preload(){
+  pic=loadImage("parrot.png")
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB)
+}
+
+function draw() {
+  background(220);
+  image(pic,0,0)
+  pic.loadPixels()
+//   noStroke gets ride of the "pen"
+  noStroke()
+  let step=1
+  for(let j=0; j<pic.height; j+=step){
+    for(let i=0; i<pic.width; i+=step){
+      let offset=(pic.width*j+i)*4
+      let pixelRed=pic.pixels[offset]
+      let pixelGreen=pic.pixels[offset+1]
+      let pixelBlue=pic.pixels[offset+2]
+      let shade=(pixelRed+pixelGreen+pixelBlue)/3
+      // fill(pixelRed, pixelGreen, pixelBlue)
+      fill(shade*frameCount%360,100,100)
+      ellipse(i,j,step*shade/200,step*shade/200)
+    }
+  }
+}
